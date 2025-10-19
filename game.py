829b1e45dc6a6ae46af2f074b663dd0c7b321bc3
ohevya <ohevya123@gmail.com>
2025-10-19 19:18:29 +0300

    self_dis is now the head with more Functionality , next we Revise flip

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from deck_ import *
-from disply import *
+from display import *
 from piles import *
 
 
@@ -8,48 +8,28 @@ def turn_deck_over(deck):
     deck.reverse()
     for card in deck:
         card.face_up = not card.face_up
-    print("Deck flipped!\n")
+    return "Deck flipped!\n"
 
 def flip(position, deck):
-
+    # need revision
     if position[0] == len(deck) - 1:
         position[0] = -1
-        turn_deck_over(deck)
+        return turn_deck_over(deck)
     elif position[0] + 3 >= len(deck):
         position[0] = len(deck) - 1
     else:
         position[0] += 3
 
-def play(position, deck, played):
 
+def can_play(position, deck):
     if position[0] != -1 and deck[position[0]].face_up == True:
-        played.append(deck[position[0]])
-        deck.pop(position[0])
-        position[0] -= 1
-        print("playing card..\n")
-    else:
-        print("can't play this card.\n")
-
-def loop():
-
-    played = []
-    deck = create_deck()
-    position = [-1]
-    piles = pile_set_up(deck)
-    foundationPiles = [[],[],[],[]]
-
-    answer_key = {
-        'q' : lambda: quit(),
-        'p' : lambda: print_deck(deck),
-
-        '0' : lambda: play(position, deck, played),
-        '1' : lambda: flip(position, deck),
-        '2' : lambda: print_played(played),
-    }
+        return True
+    return False
 
 
-    while True:
-        answer = input("\n 0: play\n 1: flip\n 2: print played\n p: print deck\n q: Quit\n :")
-        if answer in answer_key:
-            answer_key[answer]()
-        print_board(foundationPiles, piles, position, deck)
+
+def play(position, deck, piles, i):
+    played = deck[position[0]]
+    deck.pop(position[0])
+    position[0] -= 1
+    piles[i].append(played)
--- self_dis.py
+++ self_dis.py
@@ -14,7 +14,7 @@ class Display(ttk.Window):
         self.foundationPiles = FoundationPiles(foundationPiles, self)
         self.pilesW = Piles(piles, self)
         self.hand = Hand(position, deck, self)
-        # self.played = Played(played, self)
+        self.options = Play_Options(self, position, deck, piles)
 
         self.mainloop()
 
@@ -36,54 +36,76 @@ class Piles(ttk.Frame):
     def __init__(self, piles, parent):
         super().__init__(parent)
 
-        pilesW = [tk.Text(self) for _ in range(7)]
+        self.piles_data, self.self = piles, self
 
-        for i in range(7):
-            pilesW[i].insert("1.0", f"Pile {i+1}:\n")
-            for card in piles[i]:
-                if card.face_up:
-                    pilesW[i].insert("end", f"{card.card_info()[:-1]}\n")
-                else:
-                    pilesW[i].insert("end", "##\n")
-            pilesW[i].configure(width=5, height=20)
-            pilesW[i].pack(side="left", padx=5, pady=5)
+
+        self.text_piles = [tk.Text(self) for _ in range(7)]
+        self.update_piles()
+
         self.pack()
+
+    def update_piles(self):
+        for i, pile in enumerate(self.text_piles):
+            pile.delete("1.0", "end")
+            pile.insert("1.0", f"Pile {i+1}:\n")
+            for card in self.piles_data[i]:
+                if card.face_up:
+                    pile.insert("end", f"{card.card_info()[:-1]}\n")
+                else:
+                    pile.insert("end", "##\n") 
+            pile.configure(width=5, height=20)
+            pile.pack(side="left", padx=5, pady=5)     
+
 
 
 class Hand(ttk.Frame):
     def __init__(self, position, deck, parent):
         super().__init__(parent)
 
-        ttk.Button(self, text= "play", command= lambda: self.play_press(position, deck, played)).pack(side="left")
+        self.position, self.deck, self.parent, self.self = position, deck, parent, self
+        
 
-        self.card_label = ttk.Label(self, text= "start")
-        self.card_label.pack(side="left")
+        ttk.Button(self, text= "play", command= lambda: self.play_press()).pack(side="left", padx= 5)
 
-        ttk.Button(self, text= "flip", command= lambda: self.flip_press(position, deck)).pack(side="left")
+        self.card_label = ttk.Label(self, text= "none")
+        self.card_label.pack(side="left", padx= 5)
+
+        ttk.Button(self, text= "flip", command= lambda: self.flip_press()).pack(side="left", padx= 5)
+
 
         self.pack()
-    def update_card(self, position, deck):
-
-        if position[0] != -1:
-            card = deck[position[0]].card_info()
-        else:
-            card = "none"
-
-        self.card_label["text"] = card
-
-    def play_press(self, position, deck, played):
-
-        game.play(position, deck, played)
-        self.update_card(position, deck)
         
-    def flip_press(self, position, deck):
-        game.flip(position, deck)
-        self.update_card(position, deck)
+    def update_card(self, extra= None):
+
+        if self.position[0] != -1 and game.can_play(self.position, self.deck):
+            card = f"{self.deck[self.position[0]].card_info()[:-1]}\n can play"
+        elif extra:
+            card = extra
+        else:
+            card = "##\n can't play"
+
+        self.card_label["text"] = f"{card}"
+
+    def flip_press(self):
+        extra = game.flip(self.position, self.deck)
+        self.update_card(extra)
+
+    def play_press(self):
+        if game.can_play(self.position, self.deck):
+            self.parent.options.pack()
 
 
-# class Played(tk.Text):
-#     def __init__(self, played, parent):
-#         super.__init__(parent)
-#         self.played.text()
+class Play_Options(ttk.Frame):
+    def __init__(self , parent, position, deck, piles):
+        super().__init__(parent)
 
-played = []
+        self.position, self.deck, self.piles, self.parent = position, deck, piles, parent
+        
+        self.buttons = [ttk.Button(self, text= f"piles {i + 1}", command= lambda i=i: self.button_press(i)) for i in range(7)]
+        for b in self.buttons:
+            b.pack(side= "left" , padx = 5 , pady= 10)
+    def button_press(self, i):
+        game.play(self.position, self.deck, self.piles, i)
+        self.parent.options.pack_forget()
+        self.parent.hand.self.update_card()
+        self.parent.pilesW.self.update_piles()
