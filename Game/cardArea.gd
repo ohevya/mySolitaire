9a72dfa131d385c71ebf6b2ae1603cc3dbe73ff4
ohevya <ohevya123@gmail.com>
2026-03-23 12:57:39 +0200

    Implement card hover, active selection, and drag-drop logic

--- Game/cardArea.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-signal card_action(left: bool)
-
-func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action("mouseL")
-		card_action.emit(true)
-	if event.is_action("mouseR"):
-		card_action.emit(false)
